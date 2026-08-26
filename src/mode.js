3aa92f8fece13f6023106bda5ed8919f8de63f86
jojo <yanghaoleng@users.noreply.github.com>
2026-08-26 23:52:58 +0800

    fix: keep mobile call status visible

--- src/mode.js
+++ src/mode.js
@@ -12,8 +12,8 @@ let labPromise = null;
 
 function ensureLab() {
   labPromise ||= Promise.all([
-    import('./lab.js?v=20260826-voice-call'),
-    import('../vendor/calligraph-bubble.js?v=20260826-voice-call'),
+    import('./lab.js?v=20260826-voice-call-2'),
+    import('../vendor/calligraph-bubble.js?v=20260826-voice-call-2'),
   ]).then(([module]) => {
     labModule = module;
     return module;
